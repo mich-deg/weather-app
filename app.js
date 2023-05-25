85e907a8d8334589208989e50c28741518de97e5
Michael <lemichael1215@gmail.com>
2023-05-26 01:11:48 +0300

    build a weather app using axios, dotenv and express

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 import express from "express";
 
+import * as dotenv from "dotenv";
+import axios from "axios";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const apiKey = process.env.OPEN_WEATHER_API_KEY;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/weather", async (req, res) => {
+  try {
+    const city = req.query.city;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    const response = await axios.get(url);
+    const weather = response.data;
+    const message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    res.send(message);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} `);
 });
